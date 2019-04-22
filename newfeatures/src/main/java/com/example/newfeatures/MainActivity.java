abaee3bfa0b1442da3e78485abda4f8c640b8af2
josephinex <josephinex21@gmail.com>
2019-04-22 16:28:40 +0300

    Initial commit

---
+++ newfeatures/src/main/java/com/example/newfeatures/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.newfeatures;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button secondActivityButton = findViewById(R.id.secondActivity);
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntent.putExtra("com.example.newfeatures", "Hello World");
+                startActivity(startIntent);
+            }
+        });
+
+        Button googleButton = findViewById(R.id.googleBtn);
+        secondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "www.google.com";
+                Uri webAddress = Uri.parse(google);
+                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webAddress);
+                if(goToGoogle.resolveActivity(getPackageManager()) != null){
+                    startActivity(goToGoogle);
+                }
+            }
+        });
+    }
+}
